ab345962a01eb5e58beca096f84db3294689567d
Deshan <deshanprabuddha13@gmail.com>
2024-08-05 01:05:57 +0530

    developed till genereate orders

--- payment/forms.py
+++ payment/forms.py
@@ -16,3 +16,15 @@ class ShippingForm(forms.ModelForm):
         fields = ['shipping_full_name', 'shipping_email', 'shipping_address1', 'shipping_address2', 'shipping_city', 'shipping_state', 'shipping_zipcode', 'shipping_country']
 
         exclude = ['user']
+
+class BillingForm(forms.Form):
+    billing_full_name = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Name'}), required=True)
+    billing_email = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email'}), required=True)
+    billing_exp_date = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Expiration Date'}), required=True)
+    billing_cvv_number = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'CVV Number'}), required=True)
+    billing_address1 = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Address 1'}), required=True)
+    billing_address2 = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Address 2'}), required=True)
+    billing_city = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'City'}), required=True)
+    billing_state = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'State'}), required=False)
+    billing_zipcode = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Zipcode'}), required=False)
+    billing_country = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Country'}), required=True)
--- payment/views.py
+++ payment/views.py
@@ -1,8 +1,69 @@
-from django.shortcuts import render
-from cart.cart import Cart
-from payment.forms import ShippingForm
-from payment.models import ShippingAddress
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
 
+from cart.cart import Cart
+from payment.forms import ShippingForm, BillingForm
+from payment.models import ShippingAddress, Order, OrderItem
+
+def process_order(request):
+    if request.POST:
+        cart = Cart(request)
+        cart_products = cart.get_prods()
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+        
+        # get billing from the last page
+        billing_form = BillingForm(request.POST or None)
+
+        # Get shipping session data
+        my_shipping = request.session['my_shipping']
+        # Gather order info
+        full_name = my_shipping['shipping_full_name']
+        email = my_shipping['shipping_email']
+        shipping_address = f"{my_shipping['shipping_address1']}\n{my_shipping['shipping_address2']}\n{my_shipping['shipping_city']}\n{my_shipping['shipping_state']}\n{my_shipping['shipping_zipcode']}\n{my_shipping['shipping_country']}\n"
+        amount_paid = totals
+
+        # Create an order
+        if request.user.is_authenticated:
+             user = request.user
+             # create order
+             create_order = Order(user=user, full_name=full_name, email=email, address=shipping_address, amount_paid=amount_paid)
+             create_order.save()
+
+             messages.success(request, "Order Placed!")
+             return redirect('/')
+        else:
+            # guest user
+            create_order = Order(full_name=full_name, email=email, address=shipping_address, amount_paid=amount_paid)
+            create_order.save()
+
+            messages.success(request, "Order Placed!")
+            return redirect('/')
+    else:
+        messages.success(request, "Access Denied")
+        return redirect('/')
+
+def billing_info(request):
+    if request.POST:
+        cart = Cart(request)
+        cart_products = cart.get_prods()
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+
+        # Create a session with shipping info for later use
+        my_shipping = request.POST
+        request.session['my_shipping'] = my_shipping
+
+        billing_form = BillingForm()
+        if request.user.is_authenticated:
+            return render(request, 'payment/billing_info.html', {'cart_products': cart_products, 'quantities':quantities, 'totals':totals, 'shipping_info':request.POST, 'billing_form':billing_form})
+        else:
+            # guest user
+            return render(request, 'payment/billing_info.html', {'cart_products': cart_products, 'quantities':quantities, 'totals':totals, 'shipping_info':request.POST, 'billing_form':billing_form})
+    else:
+        messages.success(request, "Access Denied")
+        return redirect('/')
 
 def checkout(request):
     cart = Cart(request)
